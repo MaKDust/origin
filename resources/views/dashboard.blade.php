950b5d8ecf0029cf28a21588a4d287115a4fd089
Lord-bus <raulbus073@gmail.com>
2020-03-12 20:22:18 -0300

    tulpit, migracion producto, form editar producto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,13 +3,12 @@
  <hr class="mt-1">
 @if ((Auth::user() != null) && (Auth::user()->role == "1"))
             <div class="adminButtons ">
-                <button class="btn btn-outline-primary btn-xs"><a class="nav-link" href="{{url('/metrics')}}"><i class="fas fa-chart-bar"></i></a></button>
-                <button class="btn btn-outline-primary btn-xs"><a class="nav-link" href="{{url('/crudproducts')}}"><i class="fas fa-boxes"></i></a></button>
-                <button class="btn btn-outline-primary btn-xs"> <a class="nav-link" href="{{url('/crudusers')}}"><i class="fas fa-users"></i></a></button>
+                <button class="btn btn-outline-primary btn-xs" data-toggle="tooltip" data-placement="down" title="Metricas"><a class="nav-link" href="{{url('/metrics')}}"><i class="fas fa-chart-bar"></i></a></button>
+                <button class="btn btn-outline-primary btn-xs" data-toggle="tooltip" data-placement="down" title="Productos"><a class="nav-link" href="{{url('/crudproducts')}}"><i class="fas fa-boxes"></i></a></button>
+                <button class="btn btn-outline-primary btn-xs" data-toggle="tooltip" data-placement="down" title="Ventas"> <a class="nav-link" href="{{url('/crudusers')}}"><i class="fas fa-users"></i></a></button>
             </div>
             @endif
 @endsection
 <div class="page">
     @section('content')
 </div>
-
